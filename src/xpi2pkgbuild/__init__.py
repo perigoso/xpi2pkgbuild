b5a94ddf0579a38f3dc8cfafc5a738100c185f96
Rafael Silva <perigoso@riseup.net>
2023-04-27 17:26:16 +0100

    Project setup

---
+++ src/xpi2pkgbuild/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2023 Rafael Silva
+#
+# SPDX-License-Identifier: MIT
+
+"""Mozilla XPI based PKGBUILD generator using mozilla addon API."""
+
+__version__ = '0.1.0'
---
+++ src/xpi2pkgbuild/xpi2pkgbuild.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2023 Rafael Silva
+#
+# SPDX-License-Identifier: MIT
+
